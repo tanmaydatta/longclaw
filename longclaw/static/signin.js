d2bc757406d7f12b5c84b4306155466f4e6b227d
nirmitgoyal <nirmitgoyal.goyal@gmail.com>
2015-11-22 18:51:00 +0530

    made ratings page, fb page,added dob credential in signup & did other changes

--- longclaw/static/signin.js
+++ longclaw/static/signin.js
@@ -49,6 +49,8 @@ signinapp.controller("formctrl", ['$scope', '$http', '$element', '$cookies',
 
                     // if not successful, bind errors to error variables
                     $('#form').addClass('error');
+                    $('#form').removeClass('loading');
+
                     $('.ui.error.message').html('<ul class="list"><li>' + data['msg'] + '</li></ul>');
                     grecaptcha.reset();
                 } else {
@@ -117,6 +119,8 @@ signinapp.controller("signwithfb", ['$scope', '$http', '$cookies',
                             if (data['status'] != 'success') {
                                 // if not successful, bind errors to error variables
                                 $('#form').addClass('error');
+                                $('#form').removeClass('loading');
+
                                 $('.ui.error.message').html('<ul class="list"><li>' + data['msg'] + '</li></ul>');
                             } else {
                                 $cookies.put('auth_key', data['auth_key'], {
@@ -156,3 +160,9 @@ $('#form').form({
         }]
     }
 });
+    $(document).ready(function() {
+        $("#forgot").click(function() {
+            $('#modaldiv').modal('show');
+
+        });
+    });
